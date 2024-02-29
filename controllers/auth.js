ded878ee3923dff0eab19211ac9b3d4c230addda
EjikeOdoh <ejikeodoh2020@gmail.com>
2024-02-29 11:29:40 +0100

    Clean up codebase

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,6 @@
 const User = require("../models/User");
-const Token = require("../models/Token");
-const bcrypt = require("bcryptjs");
 
-const {
-  newUserMailer,
-  resetPasswordRequestMailer,
-} = require("../utils/mailer");
-const crypto = require("crypto");
+const { newUserMailer } = require("../utils/mailer");
 
 // Controller function to handle user registration
 const register = async (req, res) => {
@@ -91,96 +85,9 @@ const updateUser = async (req, res) => {
   }
 };
 
-// Controller function to handle user password reset request
-const resetPasswordRequest = async (req, res) => {
-  const { email } = req.body;
-
-  try {
-    // Find the user with the provided email
-    const user = await User.findOne({ email });
-
-    // If user not found, respond with an error
-    if (!user) {
-      return res.json({ status: "Error", msg: "Email not found!" });
-    }
-
-    // Delete any existing reset tokens for the user
-    let token = await Token.findOne({});
-    if (token) {
-      await token.deleteOne();
-    }
-
-    // Generate a new reset token and save it to the database
-    let resetToken = crypto.randomBytes(32).toString("hex");
-    await Token.create({
-      userId: user._id,
-      token: resetToken,
-    });
-
-    // Construct the reset password URL
-    const url = `https://mbtronics-web.vercel.app?token=${resetToken}&id=${user._id}`;
-
-    // Send a reset password request email to the user
-    resetPasswordRequestMailer(user.email, user.firstName, url);
-
-    // Respond with success status
-    return res.json({
-      status: "Success",
-    });
-  } catch (error) {
-    console.log(error);
-    // If an error occurs, respond with an error status
-    return res.json({ status: "Error", message: "Server Error" });
-  }
-};
-
-// Controller function to handle user password reset
-const resetPassword = async (req, res) => {
-  const { token, id } = req.query;
-  const { password } = req.body;
-
-  try {
-    // Find the reset token for the user
-    const resetToken = await Token.findOne({ userId: id });
-
-    // If reset token not found, respond with an error
-    if (!resetToken) {
-      return res.json({ status: "Error", message: "User not found" });
-    }
-
-    // Check if the provided token is authentic
-    const isTokenAuthentic = resetToken.compareToken(token);
-
-    // If token is not authentic, respond with an error
-    if (!isTokenAuthentic) {
-      return res.json({ status: "Error", message: "Token expired!" });
-    }
-
-    // Hash the new password and update the user's password
-    const salt = await bcrypt.genSalt(10);
-    const newPassword = await bcrypt.hash(password, salt);
-
-    // Find and update the user's password
-    const user = await User.findOneAndUpdate(
-      { _id: id },
-      { password: newPassword },
-      { new: true }
-    );
-
-    // Respond with success status
-    return res.json({ status: "Success" });
-  } catch (error) {
-    console.log(error);
-    // If an error occurs, respond with an error status
-    return res.json({ status: "Error", message: "Server Error" });
-  }
-};
-
 // Export the controller functions
 module.exports = {
   register,
   login,
   updateUser,
-  resetPasswordRequest,
-  resetPassword,
 };
--- routes/auth.js
+++ routes/auth.js
@@ -2,13 +2,7 @@
 const express = require("express");
 
 // Import the authentication controllers
-const {
-  register,
-  login,
-  updateUser,
-  resetPasswordRequest,
-  resetPassword,
-} = require("../controllers/auth");
+const { register, login, updateUser } = require("../controllers/auth");
 
 // Create an instance of the express Router
 const router = express.Router();
@@ -23,12 +17,6 @@ router.post("/login", login);
 
 // Route for updating user profile
 router.patch("/update/:id", updateUser);
-
-// Route for initiating a password reset request
-router.post("/resetpasswordrequest", resetPasswordRequest);
-
-// Route for resetting the user password
-router.post("/resetpassword", resetPassword);
 
 // Export the router for use in other parts of the application
 module.exports = router;
